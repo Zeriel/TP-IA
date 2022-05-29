bf68f0ddb01e85165e8e0d9dbf9e46c759e47007
unknown <moradillo.f@gmail.com>
2022-05-29 01:12:25 -0300

    Se agrega name = __main__ en main y utils

--- main.py
+++ main.py
@@ -1,61 +1,72 @@
+import sys
+
 from tensorflow import keras
 import pandas as pd
 import matplotlib.pyplot as plt
 
 from utils import load_data_fromlocalpath
 
-# Tomo los datasets generados previamente
-(train_images_full, train_labels_full), (test_images, test_labels) = load_data_fromlocalpath('my-datasets')
 
-# Separo las imágenes de entrenamiento en entrenamiento/validación
-valid_images, train_images = train_images_full[:50], train_images_full[50:]
-valid_labels, train_labels = train_labels_full[:50], train_labels_full[50:]
+def run_rna():
+    # Tomo los datasets generados previamente
+    (train_images_full, train_labels_full), (test_images, test_labels) = load_data_fromlocalpath('my-datasets')
 
-# Defino los labels de las clases (solo aplica para el ploteo de prueba)
-class_names = ['vacio', 'pdf417', 'otros']
+    # Separo las imágenes de entrenamiento en entrenamiento/validación
+    valid_images, train_images = train_images_full[:50], train_images_full[50:]
+    valid_labels, train_labels = train_labels_full[:50], train_labels_full[50:]
 
-# Imprimir 25 primeras imagenes mediante plot
-plt.figure(figsize=(10, 10))
-for i in range(50):
-    plt.subplot(5, 10, i + 1)
-    plt.xticks([])
-    plt.yticks([])
-    plt.grid(False)
-    plt.imshow(test_images[i], cmap=plt.cm.binary)
-    plt.xlabel(class_names[test_labels[i]])
-plt.show()
+    # Defino los labels de las clases (solo aplica para el ploteo de prueba)
+    class_names = ['vacio', 'pdf417', 'otros']
 
-valid_images, train_images, test_images = valid_images / 255., train_images / 255., test_images / 255.
+    # Imprimir 25 primeras imagenes mediante plot
+    plt.figure(figsize=(10, 10))
+    for i in range(50):
+        plt.subplot(5, 10, i + 1)
+        plt.xticks([])
+        plt.yticks([])
+        plt.grid(False)
+        plt.imshow(test_images[i], cmap=plt.cm.binary)
+        plt.xlabel(class_names[test_labels[i]])
+    plt.show()
 
-# Se define el modelo de la RNA con sus layers y neuronas
-model = keras.models.Sequential([keras.layers.Flatten(input_shape=[28, 28]),
-                                 keras.layers.Dense(300, activation='relu'),
-                                 keras.layers.Dense(100, activation='relu'),
-                                 keras.layers.Dense(100, activation='relu'),
-                                 keras.layers.Dense(100, activation='relu'),
-                                 keras.layers.Dense(10, activation='softmax')])
+    valid_images, train_images, test_images = valid_images / 255., train_images / 255., test_images / 255.
 
-# Se muestra cómo quedó definido el modelo
-model.summary()
+    # Se define el modelo de la RNA con sus layers y neuronas
+    model = keras.models.Sequential([keras.layers.Flatten(input_shape=[28, 28]),
+                                     keras.layers.Dense(300, activation='relu'),
+                                     keras.layers.Dense(100, activation='relu'),
+                                     keras.layers.Dense(100, activation='relu'),
+                                     keras.layers.Dense(100, activation='relu'),
+                                     keras.layers.Dense(10, activation='softmax')])
 
-# Se compila el modelo
-model.compile(loss='sparse_categorical_crossentropy',
-              optimizer='sgd',
-              metrics=['accuracy'])
+    # Se muestra cómo quedó definido el modelo
+    model.summary()
 
-# Se define el dataset de entrenamiento junto con sus labels. Se agrega también el dataset de validación y se definen
-# 10 épocas (iteraciones)
-history = model.fit(train_images,
-                    train_labels,
-                    epochs=10,
-                    validation_data=(valid_images, valid_labels))
+    # Se compila el modelo
+    model.compile(loss='sparse_categorical_crossentropy',
+                  optimizer='sgd',
+                  metrics=['accuracy'])
 
-# Se muestran los resultados de las iteraciones, en forma de texto y de gráfico cartesiano
-pd.DataFrame(history.history).plot(figsize=(16, 10))
-plt.grid(True)
-plt.gca().set_ylim(0, 1)
-plt.show()
+    # Se define el dataset de entrenamiento junto con sus labels. Se agrega también el dataset de validación y se
+    # definen 10 épocas (iteraciones)
+    history = model.fit(train_images,
+                        train_labels,
+                        epochs=10,
+                        validation_data=(valid_images, valid_labels))
 
-# Con lo anterior, se tiene al modelo entrenado. Se procede a ejecutar la evaluación con el dataset de prueba
-print('Evaluación final: ')
-model.evaluate(test_images, test_labels)
+    # Se muestran los resultados de las iteraciones, en forma de texto y de gráfico cartesiano
+    pd.DataFrame(history.history).plot(figsize=(16, 10))
+    plt.grid(True)
+    plt.gca().set_ylim(0, 1)
+    plt.show()
+
+    # Con lo anterior, se tiene al modelo entrenado. Se procede a ejecutar la evaluación con el dataset de prueba
+    print('Evaluación final: ')
+    model.evaluate(test_images, test_labels)
+
+
+if __name__ == "__main__":
+    run_rna()
+else:
+    print("Este script debe ejecutarse como como programa principal, no como módulo importado")
+    sys.exit(1)
--- utils.py
+++ utils.py
@@ -1,10 +1,12 @@
 import os
+import sys
+
 import numpy as np
 import gzip
 
 
 def load_data_fromlocalpath(input_path):
-  """Loads the Fashion-MNIST dataset.
+    """Loads the Fashion-MNIST dataset.
   Modified by Henry Huang in 2020/12/24.
   We assume that the input_path should in a correct path address format.
   We also assume that potential users put all the four files in the path.
@@ -14,28 +16,32 @@ def load_data_fromlocalpath(input_path):
   Returns:
       Tuple of Numpy arrays: `(x_train, y_train), (x_test, y_test)`.
   """
-  files = [
-      'train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz',
-      'test-labels-idx1-ubyte.gz', 'test-images-idx3-ubyte.gz'
-  ]
+    files = [
+        'train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz',
+        'test-labels-idx1-ubyte.gz', 'test-images-idx3-ubyte.gz'
+    ]
 
-  paths = []
-  for fname in files:
-    paths.append(os.path.join(input_path, fname))  # The location of the dataset.
+    paths = []
+    for fname in files:
+        paths.append(os.path.join(input_path, fname))  # The location of the dataset.
+
+    with gzip.open(paths[0], 'rb') as lbpath:
+        y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+
+    with gzip.open(paths[1], 'rb') as imgpath:
+        x_train = np.frombuffer(
+            imgpath.read(), np.uint8, offset=16).reshape(len(y_train), 28, 28)
+
+    with gzip.open(paths[2], 'rb') as lbpath:
+        y_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
+
+    with gzip.open(paths[3], 'rb') as imgpath:
+        x_test = np.frombuffer(
+            imgpath.read(), np.uint8, offset=16).reshape(len(y_test), 28, 28)
+
+    return (x_train, y_train), (x_test, y_test)
 
 
-  with gzip.open(paths[0], 'rb') as lbpath:
-    y_train = np.frombuffer(lbpath.read(), np.uint8, offset=8)
-
-  with gzip.open(paths[1], 'rb') as imgpath:
-    x_train = np.frombuffer(
-        imgpath.read(), np.uint8, offset=16).reshape(len(y_train), 28, 28)
-
-  with gzip.open(paths[2], 'rb') as lbpath:
-    y_test = np.frombuffer(lbpath.read(), np.uint8, offset=8)
-
-  with gzip.open(paths[3], 'rb') as imgpath:
-    x_test = np.frombuffer(
-        imgpath.read(), np.uint8, offset=16).reshape(len(y_test), 28, 28)
-
-  return (x_train, y_train), (x_test, y_test)
+if __name__ == "__main__":
+    print("Este script debe ejecutarse como módulo importado, no como programa principal")
+    sys.exit(1)
